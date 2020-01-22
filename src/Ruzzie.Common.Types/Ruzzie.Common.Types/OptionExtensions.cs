bc9a478558a6037f3f1060d552865099ea038faf
Ruzzie <dorus.verhoeckx@gmail.com>
2020-01-22 07:40:42 +0100

    [impr] Improved doc

--- src/Ruzzie.Common.Types/Ruzzie.Common.Types/OptionExtensions.cs
+++ src/Ruzzie.Common.Types/Ruzzie.Common.Types/OptionExtensions.cs
@@ -9,8 +9,9 @@ namespace Ruzzie.Common.Types
             return new Option<TValue>(value);
         }
 
-        /////Applies a function to the contained value (if any), or returns the provided default.
-        //TU MapOr<TU>(TU @default, Func<T, TU> mapResultTo);
+        /// <summary>
+        /// Applies a function to the contained value (if any), or returns the provided default.
+        /// </summary>
         public static T MapOr<T, TValue>(this Option<TValue> self, T @default, Func<TValue, T> mapValueTo)
         {
             return self.Match(ReturnDefault, mapValueTo);
